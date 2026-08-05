86d3ead506dc05b08b36b28543a8576d53b325e3
marsdev0 <2277087813@qq.com>
2026-08-05 23:10:08 +0800

    feat: bootstrap Spring Boot WebFlux project scaffold

---
+++ src/main/java/com/marsdev/janus/JanusApplication.java
@@ -0,0 +1,13 @@
+package com.marsdev.janus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JanusApplication.class, args);
+    }
+
+}
